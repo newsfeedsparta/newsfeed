9925c81df1414e3f88db98185449ba610c06f0a6
ethrad <asdf123f4f4@gmail.com>
2024-10-22 17:56:58 +0900

    Refactor: 필요 없는 Setter 제거

--- src/main/java/com/sparta/newsfeed/dto/user/SignupRequestDto.java
+++ src/main/java/com/sparta/newsfeed/dto/user/SignupRequestDto.java
@@ -3,7 +3,6 @@ package com.sparta.newsfeed.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class SignupRequestDto {
